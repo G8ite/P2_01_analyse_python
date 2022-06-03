8b84568e219476dc0cb0526a14c9eaba9aee589e
Amandine Pimentel de Andrade <70423300+G8ite@users.noreply.github.com>
2022-06-03 13:23:46 +0200

    without info print

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ data = ['product_page_url', 'universal_ product_code (upc)', 'title',
 'category', 'review_rating', 'image_url']
 
 info, names = all_categories_details('https://books.toscrape.com/index.html')
-print(f"taille des infos : {len(info)}")
 
 # with csv lib
 for category in info :
